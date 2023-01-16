faf7aa70b3a7008257f8574418353a89dbb7bdd4
Rie0531 <113961502+Rie0531@users.noreply.github.com>
2023-01-16 17:22:37 +0900

    プロトタイプ編集機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,6 +22,19 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
   end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit
+    end
+  end
  
 
   private
